1913525a493d61ddc8d24a364ec531dbe167887b
Paul Wolfgang <wolfgang@temple.edu>
2018-06-21 09:47:17 -0400

    Initial commit. Program is close to Mallet performance.

---
+++ src/main/java/edu/temple/cla/papolicy/wolfgang/texttools/classifynaivebayes/Main.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2018, Temple University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * All advertising materials features or use of this software must display 
+ *   the following  acknowledgement
+ *   This product includes software developed by Temple University
+ * * Neither the name of the copyright holder nor the names of its 
+ *   contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.temple.cla.papolicy.wolfgang.texttools.classifynaivebayes;
+
+import edu.temple.cla.papolicy.wolfgang.texttools.util.CommonFrontEnd;
+import edu.temple.cla.papolicy.wolfgang.texttools.util.Vocabulary;
+import edu.temple.cla.papolicy.wolfgang.texttools.util.WordCounter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+import picocli.CommandLine;
+
+/**
+ *
+ * @author Paul
+ */
+public class Main implements Callable<Void> {
+
+    @CommandLine.Option(names = "--output_table_name",
+            description = "Table where results are written")
+    private String outputTableName;
+
+    @CommandLine.Option(names = "--output_code_col", required = true,
+            description = "Column where the result is set")
+    private String outputCodeCol;
+
+    @CommandLine.Option(names = "--model",
+            description = "Directory where model files are written")
+    private String modelDir = "Model_Dir";
+
+    private final String[] args;
+
+    public Main(String[] args) {
+        this.args = args;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main(args);
+        CommandLine commandLine = new CommandLine(main);
+        commandLine.setUnmatchedArgumentsAllowed(true).parse(args);
+        try {
+            main.call();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public Void call() {
+        List<String> ids = new ArrayList<>();
+        List<String> ref = new ArrayList<>();
+        List<WordCounter> counts = new ArrayList<>();
+        List<Integer> cats = new ArrayList<>();
+        Vocabulary problemVocab = new Vocabulary(); // Not used
+        CommonFrontEnd commonFrontEnd = new CommonFrontEnd();
+        CommandLine commandLine = new CommandLine(commonFrontEnd);
+        commandLine.setUnmatchedArgumentsAllowed(true);
+        commandLine.parse(args);
+        commonFrontEnd.loadData(ids, ref, problemVocab, counts);
+        File modelParent = new File(modelDir);
+        Map<String, Double> prior
+                = (Map<String, Double>) readFile(modelParent, "prior.bin");
+        Map<String, Map<String, Double>> condProb
+                = (Map<String, Map<String, Double>>) readFile(modelParent, "condProp.bin");
+        List<Integer> categories = new ArrayList<>();
+        for (int i = 0; i < counts.size(); i++) {
+            Integer cat = classify(counts.get(i), prior, condProb);
+            categories.add(cat);
+        }
+        String outputTable = outputTableName != null ? outputTableName : commonFrontEnd.getTableName();
+        if (outputCodeCol != null) {
+            System.err.println("Inserting result into database");
+            commonFrontEnd.outputToDatabase(outputTable,
+                    outputCodeCol,
+                    ids,
+                    categories);
+        }
+        System.err.println("SUCESSFUL COMPLETION");
+
+        return null;
+    }
+
+    public Object readFile(File modelParent, String name) {
+        File inputFile = new File(modelParent, name);
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFile))) {
+            return ois.readObject();
+        } catch (ClassNotFoundException | IOException ioex) {
+            throw new RuntimeException("Error reading " + inputFile.getPath(), ioex);
+        }
+    }
+
+    public Integer classify(WordCounter counter, Map<String, Double> prior, Map<String, Map<String, Double>> condProb) {
+        SortedMap<Double, String> testCats = new TreeMap<>();
+        prior.forEach((cat, priorProb) -> {
+            double score = Math.log(priorProb);
+            score += counter.getWords().stream()
+                    .collect(Collectors.summingDouble(word -> {
+                        int countOfWord = counter.getCount(word);
+                        if (condProb.containsKey(word)) {
+                            double term = Math.log(condProb.get(word).get(cat)) * countOfWord;
+                            return term;
+                        } else {
+                            return 0;
+                        }
+                    }));
+            testCats.put(score, cat);
+        });
+        String winningCat = testCats.get(testCats.lastKey());
+        return new Integer(winningCat);
+    }
+}
---
+++ src/test/java/edu/temple/cla/papolicy/wolfgang/texttools/classifynativebayes/MainTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2018, Temple University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * All advertising materials features or use of this software must display 
+ *   the following  acknowledgement
+ *   This product includes software developed by Temple University
+ * * Neither the name of the copyright holder nor the names of its 
+ *   contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package edu.temple.cla.papolicy.wolfgang.texttools.classifynativebayes;
+
+import edu.temple.cla.papolicy.wolfgang.texttools.classifynaivebayes.Main;
+import edu.temple.cla.papolicy.wolfgang.texttools.util.CommonFrontEnd;
+import edu.temple.cla.papolicy.wolfgang.texttools.util.Vocabulary;
+import edu.temple.cla.papolicy.wolfgang.texttools.util.WordCounter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import org.junit.Before;
+import org.junit.Test;
+import picocli.CommandLine;
+
+/**
+ *
+ * @author Paul
+ */
+public class MainTest {
+    
+    List<String> ref = new ArrayList<>();
+    List<String> ids = new ArrayList<>();
+    Vocabulary vocabulary = new Vocabulary();
+    List<WordCounter> counts = new ArrayList<>();
+    String[] args = {"--datasource", "TestDb.txt",
+                "--table_name", "TestTableUnknown",
+                "--id_column", "ID",
+                "--text_column", "Abstract",
+                "--code_column", "Code",
+                "--remove_stopwords", "false",
+                "--output_code_col", "Code"};
+    Map<String, Integer> docsInTrainingSet = new TreeMap<>();
+    Map<String, WordCounter> trainingSets = new TreeMap<>();
+    Map<String, Double> prior = new TreeMap<>();
+    Map<String, Map<String, Double>> condProb = new TreeMap<>();
+
+    public MainTest() {
+    }
+
+    @Before
+    public void Setup() {
+        TestDatabase.createTestTable();
+    }
+    
+    
+    
+    @Test
+    public void testMain() {
+        Main.main(args);
+    }
+
+    
+}
